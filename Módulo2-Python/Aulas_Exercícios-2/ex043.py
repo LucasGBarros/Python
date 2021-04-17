79c2c25bff4e75cd3420c6a154dce877a7dcbd8c
Lucas Gomes <Lucasgomesdebarros99@gmail.com>
2021-04-16 22:23:03 -0300

    Commit

--- Módulo2-Python/Aulas_Exercícios-2/ex043.py
+++ Módulo2-Python/Aulas_Exercícios-2/ex043.py
@@ -14,27 +14,30 @@ de acordo com a tabela abaixo:
 
 print('='*50)
 
-peso = float(input('Digite seu peso: (Kg) '))
+peso = float(input('Digite seu peso: '))
 altura = float(input('Digite a sua Altura: '))
 
-imc = peso / (altura * altura)
+imc = peso / (altura * altura) # Ou "imc = peso (altura ** 2)
 
 if imc <= 18.5:
 
-    print('Seu índice de massa corporal é de {:.2f}. Você está abaixo do peso.'.format(imc))
+    print('Seu índice de massa corporal é de {:.1f}. Você está abaixo do peso.'.format(imc))
 
-elif (imc >= 18.5) and (imc <= 25):
+elif (imc >= 18.5) and (imc <= 25): # Ou podendo ser "18.5 <= imc < 25"
 
-    print('Seu índice de massa corporal é de {:.2f}. Peso ideal para o seu corpo. Parabens!!'.format(imc))
+    print('Seu índice de massa corporal é de {:.1f}. Peso ideal para o seu corpo. Parabéns!!'.format(imc))
 
-elif (imc >= 25) and (imc <= 30):
+elif (imc >= 25) and (imc <= 30): # Ou podendo ser  "25 <= imc < 30"
 
-    print('Seu índice de massa corporal é de {:.2f}. Você está Sobrepeso'.format(imc))
+    print('Seu índice de massa corporal é de {:.1f}. Você está Sobrepeso'.format(imc))
 
-elif (imc >= 30) and (imc <= 40):
+elif (imc >= 30) and (imc <= 40): # Ou podendo ser "30 <= imc <40
 
-    print('Seu índice de massa corporal é de {:.2f}. Você está na Obesidade.'.format(imc))
+    print('Seu índice de massa corporal é de {:.1f}. Você está na Obesidade.'.format(imc))
 
 else:
 
-    print('Seu índice de massa corporal é de {:.2f}. Você está na Obesidade Mórbida.'.format(imc))
+    print('Seu índice de massa corporal é de {:.1f}. Você está na Obesidade Mórbida.'.format(imc))
+
+print('='*50)
+
