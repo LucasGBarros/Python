cb34cbb4cbc472efa89c598aebf967ed3fa76bb8
Lucas Gomes <Lucasgomesdebarros99@gmail.com>
2021-04-16 22:08:11 -0300

    Commit

---
+++ Módulo2-Python/Aulas_Exercícios-2/ex043.py
@@ -0,0 +1,40 @@
+print("""Desenvolva uma lógica que leia o peso e a aultura de uma pessoa, calcule seu IMC e ostre seu sttatus,
+de acordo com a tabela abaixo:
+
+- Abaixo de 18.5: abaixo do peso.
+
+- Entre 18.5 e 25: peso ideal.
+
+- 25 até 30: Sobrepeso.
+
+- 30 até 40: Obesidade.
+
+- Acima de 40: Obesidade Mórbida.""")
+
+
+print('='*50)
+
+peso = float(input('Digite seu peso: (Kg) '))
+altura = float(input('Digite a sua Altura: '))
+
+imc = peso / (altura * altura)
+
+if imc <= 18.5:
+
+    print('Seu índice de massa corporal é de {:.2f}. Você está abaixo do peso.'.format(imc))
+
+elif (imc >= 18.5) and (imc <= 25):
+
+    print('Seu índice de massa corporal é de {:.2f}. Peso ideal para o seu corpo. Parabens!!'.format(imc))
+
+elif (imc >= 25) and (imc <= 30):
+
+    print('Seu índice de massa corporal é de {:.2f}. Você está Sobrepeso'.format(imc))
+
+elif (imc >= 30) and (imc <= 40):
+
+    print('Seu índice de massa corporal é de {:.2f}. Você está na Obesidade.'.format(imc))
+
+else:
+
+    print('Seu índice de massa corporal é de {:.2f}. Você está na Obesidade Mórbida.'.format(imc))
