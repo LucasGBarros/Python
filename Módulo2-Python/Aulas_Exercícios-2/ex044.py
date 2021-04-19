57550651763968efd2204ac41ed2f7c2db6d7472
Lucas Gomes <Lucasgomesdebarros99@gmail.com>
2021-04-19 20:44:50 -0300

    Commit

--- Módulo2-Python/Aulas_Exercícios-2/ex044.py
+++ Módulo2-Python/Aulas_Exercícios-2/ex044.py
@@ -8,50 +8,48 @@ condição de pagamento:
 
 print('{:=^40}'.format('MERCADO'))
 
-produto = float(input('Qual o valor do produto? '))
+preço = float(input('Qual o valor do produto? R$ '))
 
-print("""O valor do produto é R$ {:.2f}, qual vai ser a a forma de pagamento?
+print('''FORMA DE PAGAMENTO
 [1] DINHEIRO / CHEQUE
 [2] Débito
 [3] 2x no cartão
-[4] 3x ou mais no cartão
-""".format(produto))
+[4] 3x ou mais no cartão''')
 
-opcões = int(input('Digite a Opção que deseja: '))
+opção = int(input('Qual é a opção? '))
 
-if opcões == 1:
+if opção == 1:
 
-    total = produto - (produto * 25 / 100)
+    total = preço - (preço * 10 / 100)
 
-elif opcões == 2:
+elif opção == 2:
 
-    total = produto - (produto * 5 / 100)
+    total = preço - (preço * 5 / 100)
 
+elif opção == 3:
 
-elif opcões == 3:
+    total = preço
 
-    total = produto
+    parcela = total / 2
 
-    parcela = produto / 2
+    print('Sua compra será parcelada em 2x de R$ {:.2f} SEM JUROS'.format(parcela))
 
-    print('Sua compra será parcelada em 2x de R$ {}. SEM JUROS'.format(parcela))
+elif opção == 4:
 
-elif opcões == 4:
+    total = preço + (preço * 20 / 100)
 
-    produto - (produto * 25 / 100)
+    totalparc = int(input('Quantas Parcelas? '))
 
-    parcelas = int(input('Quantas parcelas: '))
+    parcela = total / totalparc
 
-    parceljuros = produto / parcelas
-
-    print('Sua compra será parcelada em {:.0f}x de R$ {:.2f} COM JUROS.'.format(parcelas, parceljuros))
+    print('Sua compra será parcelada em {}x de R$ {:.2f} COM JUROS'.format(totalparc, parcela))
 
 else:
 
-    opcões = 0
+    total = 0
 
-    print('Opção Invalida. Tente Novamente.')
+    print('Opção inválida de pagamento. Tente novamente!')
 
-print('Sua compra de R$ {:.2f} vai custar R$ {:.2f} no final.'.format(produto, total))
+print('Sua compra de R$ {:.2f}, vai custar R$ {:.2f} no final.'.format(preço, total))
 
 print('{:=^40}'.format('VOLTE SEMPRE'))
