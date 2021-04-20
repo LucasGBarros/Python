7bd4ca69abe034f689511df6648b1f8d3dd76d43
Lucas Gomes <Lucasgomesdebarros99@gmail.com>
2021-04-19 21:34:06 -0300

    Commit

--- Módulo2-Python/Aulas_Exercícios-2/ex045.py
+++ Módulo2-Python/Aulas_Exercícios-2/ex045.py
@@ -1,10 +1,81 @@
-pessoas = int(input('Numero de pessoa: '))
+print('Crie um programa que faça o computador jogar Jokenpô com você.')
 
-pessoas = 60 *25 / 100
+from random import randint
 
-resultado = pessoas
+itens = ('PEDRA', 'PAPEL', 'TESOURA')
 
-pessoas2 = 60 * 20 / 100
+computador = randint(0, 2)
 
-print(resultado)
-print(pessoas2)
+print('''Suas Jogadas:
+
+[0] PEDRA
+[1] PAPEL
+[2] TESOURA
+''')
+
+jogador = int(input('Qual é a sua jogada? '))
+
+print('=-'*25)
+
+print('O Computador escolheu {}.'.format(itens[computador]))
+
+print('O Jogador jogou {}.'.format(itens[jogador]))
+
+
+print('=-'*25)
+
+if computador == 0: # Computador jogou Pedra.
+
+    if jogador == 0:
+
+        print('EMPATE')
+
+    elif jogador == 1:
+
+        print('Jogador Venceu')
+
+    elif jogador == 2:
+
+        print('Computador Venceu')
+
+    else:
+
+        print('Jogada Inválida.')
+
+elif computador == 1:  # Computador jogou Papel.
+
+    if jogador == 0:
+
+        print('Computador Venceu.')
+
+    elif jogador == 1:
+
+        print('Empate.')
+
+    elif jogador == 2:
+
+        print('Jogador venceu.')
+
+    else:
+
+        print('Jogada Inválida')
+
+elif computador == 2:   # Computador jogou Tesoura.
+
+    if jogador == 0:
+
+        print('Jogador Venceu.')
+
+    elif jogador == 1:
+
+        print('Computador venceu.')
+
+    elif jogador == 2:
+
+        print('Empate.')
+
+    else:
+
+        print('Jogada Inválida.')
+
+print('=-'*25)
