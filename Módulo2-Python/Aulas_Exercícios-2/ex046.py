7821470eff366beec9e499ce71deaf18c2f93d83
Lucas Gomes <Lucasgomesdebarros99@gmail.com>
2021-04-21 11:46:06 -0300

    Commit

---
+++ Módulo2-Python/Aulas_Exercícios-2/ex046.py
@@ -0,0 +1,16 @@
+print("""Faça um programa que mostre na tela uma contagem regressiva para estouro de fogos de artificio, indo de 10 a 0,
+com uma pausa de 1 segundo entre eles.""")
+
+import emoji
+
+from time import sleep
+
+print('Vai começar a contagem regressiva...')
+
+for c in range(10, 0, -1):
+
+    sleep(1)
+
+    print(c)
+
+print(emoji.emojize(':fireworks:'))
