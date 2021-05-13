1357615c863b3974eda4a73c40085fa872123049
Lucas Gomes <Lucasgomesdebarros99@gmail.com>
2021-05-12 22:10:06 -0300

    Realização de Exercício
    Ex071

--- Módulo2-Python/Aulas_Exercícios-2/ex071.py
+++ Módulo2-Python/Aulas_Exercícios-2/ex071.py
@@ -4,3 +4,52 @@ valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas 
 OBS: Considere que o caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$ 1
 ''')
 
+print('='*25)
+
+print(f'{"BANCO":^25}')
+
+print('='*25)
+
+valor = int(input('Qual valor você quer sacar: R$ '))
+
+total = valor
+
+céd = 50
+
+totCéd = 0
+
+while True:
+
+    if total >= céd:
+
+        total -= céd
+
+        totCéd += céd
+
+    else:
+
+        if totCéd > 0:
+
+            print(f'Total de {totCéd} cédulas de R$ {céd}.')
+
+        if céd == 50:
+
+            céd = 20
+
+        elif céd ==20:
+
+            céd = 10
+
+        elif céd == 10:
+
+            céd = 1
+
+        totCéd = 0
+
+        if total == 0:
+
+            break
+
+
+print(f'{" VOLTE SEMPRE ":=^25}')
+
