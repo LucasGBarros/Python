1582bad38d4738320e7b699405e41c08ca1aecf6
Lucas Gomes <Lucasgomesdebarros99@gmail.com>
2021-05-22 19:33:14 -0300

    Update Formulário
    Estado Civil

--- Projeto/Formulário.py
+++ Projeto/Formulário.py
@@ -10,49 +10,146 @@ menu = 0
 
 nome = str(input('Nome: '))
 
-while menu != 7:
+while True:
 
-    print('''
+    while menu != 6:
+
+        print('''
 Olá {}, tudo bem?
-
+    
 Em qual quadro se encaixa a sua idade?
-
+    
 [1] 18 - 20 ANOS;
 [2] 21 - 30 ANOS;
 [3] 31 - 40 ANOS;
 [4] 41 - 50 ANOS;
 [5] 51 - 60 ANOS;
 [6] 61 ou mais;
-[7] FECHAR PROGRAMA!
-'''.format(nome.strip().title()))
+    '''.format(nome.strip().title()))
 
-    menu = int(input('Digite a sua opção: '))
+        menu = int(input('Digite a sua opção: '))
 
-    if menu == 1 or menu == 2 or menu == 3 or menu == 4 or menu == 5 or menu == 6:      # Informando Quadro de Sexo
+        if menu == 1 or menu == 2 or menu == 3 or menu == 4 or menu == 5 or menu == 6:      # Informando Quadro de Sexo
 
-        print('''Ok, carregando informações...''')
+            sleep(1)
 
-        sleep(2)
+            print('''Ok, carregando informações...''')
 
-        sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+            sleep(2)
 
-        while sexo not in 'MmFf':
+            sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]    # Utilizando métodos de String e Fatiamento.
 
-            sexo = str(input('Dados inválidos, digite novamente seu sexo: [M/F] '))
+            while sexo not in 'MmFf':
 
-        print(f'Sexo {sexo.upper()} registrado com sucesso.')
+                sexo = str(input('Dados inválidos, digite novamente seu sexo: [M/F] ')) # Informando sexo correto
 
-        break
+            sleep(1)
 
-    if menu != 7:
+            print(f'Sexo {sexo.upper()} registrado com sucesso.')
 
-        print('Opção inválida! Tente novamente!')
+            break
+
+        elif menu != 6:   # Tentando novamente
+
+            print('Opção inválida! Tente novamente!')
+
+    sleep(1)
+
+    menu = 0
+
+    while menu != 4:
+
+        sleep(1)
+
+        print(f'{" Estado Civil ":=^40}')
+
+        sleep(1)
+
+        print('''
+Informe seu Estado Civil
+        
+[1] Solteiro.
+[2] Casado.
+[3] Divorciado.
+[4] Viúvo.
+        ''')
+
+        menu = int(input('Qual a sua opção? '))
 
 
-sleep(1.5)
+        if menu == 1:
 
-print('Finalizando...')
+            sleep(1)
 
-sleep(2)
+            print('Carregando dados...')
 
-print('Formulário Encerrado!')
+            sleep(1)
+
+            menu = 'Solteiro'
+
+            print(f'Seu Estado Civil {menu} foi registrado com sucesso.')
+
+            break
+
+        elif menu == 2:
+
+            sleep(1)
+
+            print('Carregando dados...')
+
+            sleep(1)
+
+            menu = 'Casado'
+
+            print(f'Seu estado Civil {menu} foi regstrado com sucesso.')
+
+            break
+
+        elif menu == 3:
+
+            sleep(1)
+
+            print('Carregando dados...')
+
+            sleep(1)
+
+            menu = 'Divorciado'
+
+            print(f'Seu Estado Civil {menu} foi registrado com sucesso.')
+
+            break
+
+        elif menu == 4:
+
+            sleep(1)
+
+            print('Carregando dados...')
+
+            sleep(1)
+
+            menu = 'Viúvo'
+
+            print(f'Seu Estado Civil {menu} foi registrado com sucesso.')
+
+            break
+
+        if menu != 4:
+
+            sleep(1)
+
+            print(f'{" Estado Civil ":=^40}')
+
+            sleep(1)
+
+            print("""
+Informe seu Estado Civil
+                    
+[1] Solteiro.
+[2] Casado.
+[3] Divorciado.
+[4] Viúvo.
+    """)
+
+        menu = int(input('Qual a sua opção? '))
+
+    break
