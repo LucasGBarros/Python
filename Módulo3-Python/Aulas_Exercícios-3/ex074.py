054692097087bdc57475f35b1cff3652644f36e3
Lucas Gomes <Lucasgomesdebarros99@gmail.com>
2021-06-06 18:16:58 -0300

    Realização de Exercícios

--- Módulo3-Python/Aulas_Exercícios-3/ex074.py
+++ Módulo3-Python/Aulas_Exercícios-3/ex074.py
@@ -14,6 +14,6 @@ for n in numeros:
 
     print(f'{n} ', end='')
 
-print(f'\nO maior valor ssorteado foi {max(numeros)}')
+print(f'\nO maior valor sorteado foi {max(numeros)}')
 
 print(f'O menor valor sorteado foi {min(numeros)}')
